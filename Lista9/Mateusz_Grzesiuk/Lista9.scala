533afc38a78226f27a5ab77dcde5c425a00aa4ac
Mateusz Grzesiuk <mateusz.grzesiuk@op.pl>
2020-12-13 00:36:19 +0100

    Add files via upload

--- Lista9/Mateusz_Grzesiuk/Lista9.scala
+++ Lista9/Mateusz_Grzesiuk/Lista9.scala
@@ -32,47 +32,56 @@ object Time1{
 
 // a)
 
-class Time2A(protected var godz:Int)(protected var min:Int) {
-  require(godz <24, s"godz=$godz")
-  if(godz < 0) godz = 0
-  require(min <60, s"min=$min")
-  if(min < 0) min = 0
+class Time2A(protected var _godz:Int)(protected var _min:Int) {
+  require(_godz <24, s"godz="+_godz)
+  if(_godz < 0) _godz = 0
+  require(_min <60, s"min="+_min)
+  if(_min < 0) _min = 0
 
-  def hour:(Int, Int) = (godz, min)
-  def hour_= (newGodz: Int, newMin: Int): Unit ={
-    require(newGodz <24, s"newh=$newGodz")
-    if(newGodz>=0) godz = newGodz
-    else godz = 0
-    require(newMin <60, s"newm=$newMin")
-    if(newMin>=0) min = newMin
-    else min = 0
+  def godz:Int = _godz
+  def godz_= (newGodz: Int): Unit ={
+    require(newGodz <24, s"newGodz=$newGodz")
+    if(newGodz>=0) _godz = newGodz
+    else _godz = 0
+  }
+
+  def min:Int = _min
+  def min_= (newMin: Int): Unit ={
+    require(newMin <60, s"newMin=$newMin")
+    if(newMin>=0) _min = newMin
+    else _min = 0
   }
 
   def before(other: Time2A): Boolean ={
-    hour._1<other.hour._1 || (hour._1==other.hour._1 && hour._2<other.hour._2)
+    godz<other.godz || (godz==other.godz && min<other.min)
   }
 }
 
 // b)
 
-class Time2B(godz:Int)(protected var min:Int) {
+class Time2B(_godz:Int)(protected var _min:Int) {
 
-  require(min <60, s"min=$min")
-  if(min < 0) min = 0
-  require(godz <24, s"godz=$godz")
-  if(godz > 0) min = min+godz*60
+  require(_min <60, s"min="+_min)
+  if(_min < 0) _min = 0
+  require(_godz <24, s"godz="+_godz)
+  if(_godz > 0) _min = _min+_godz*60
 
-  def hour:(Int, Int) = (min%60, min-min%60)
-  def hour_= (newGodz: Int, newMin: Int): Unit ={
-    require(newGodz <24, s"newh=$newGodz")
-    if(newGodz>=0) min = newGodz*60
-    else min = 0
-    require(newMin <60, s"newm=$newMin")
-    if(newMin>=0) min = min + newMin
+  def godz:Int = _min/60
+  def godz_= (newGodz: Int): Unit ={
+    require(newGodz <24, s"newGodz=$newGodz")
+    if(newGodz>=0) _min=_min%60+newGodz*60
+    else _min=_min%60
+  }
+
+  def min:Int = _min%60
+  def min_= (newMin: Int): Unit ={
+    require(newMin <60, s"newMin=$newMin")
+    if(newMin>=0) _min = _min-_min%60+newMin
+    else _min = _min-_min%60
   }
 
   def before(other: Time2B): Boolean ={
-    hour._1<other.hour._1 || (hour._1==other.hour._1 && hour._2<other.hour._2)
+    godz<other.godz || (godz==other.godz && min<other.min)
   }
 }
 
@@ -122,6 +131,12 @@ object Lista9 {
     val time5 = new Time2B(10)(8)
     println(time5.before(time4))
     println(!time4.before(time5))
+    println(time5.godz==10)
+    println(time5.min==8)
+    time5.godz = 2
+    time5.min = 37
+    println(time5.godz==2)
+    println(time5.min==37)
     //Zadanie 3
     val pojazd = new Pojazd("BMW", "Z4")
     val pojazd2 = new Pojazd("Audi", "R8", 2019)
